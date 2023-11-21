6d6536de6a7814d535d7ab3f49b46ea1186c4886
simsimanmarvin <114800209+simsimanmarvin@users.noreply.github.com>
2023-11-21 17:38:32 +0800

    Update TypeofFunctionsAVAXM3.sol

--- TypeofFunctionsAVAXM3.sol
+++ TypeofFunctionsAVAXM3.sol
@@ -1,24 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
 
 // Inherit from ERC20 and Ownable
-contract MyToken is ERC20, Ownable {
+contract UniqueToken is ERC20, Ownable {
     constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol) {
-        _mint(msg.sender, _initialSupply);
+        // Mint initial supply to the owner
+        _mint(_msgSender(), _initialSupply);
     }
 
-    // Function to mint new tokens
-    function mint(address _to, uint256 _value) public onlyOwner {
-        _mint(_to, _value); 
+    // Custom function to mint new tokens
+    function issueTokens(address _to, uint256 _amount) public onlyOwner {
+        // Mint tokens to the recipient
+        _mint(_to, _amount);
     }
 
-    // Function to burn existing tokens
-    function burn(uint256 _value) public  {
-        _burn(msg.sender, _value);
+    // Custom function to burn existing tokens
+    function destroyTokens(uint256 _amount) public {
+        // Burn tokens from the sender's account
+        _burn(_msgSender(), _amount);
     }
+
     function transfer(address _to, uint256 _value) public override returns (bool success) {
+        // Transfer tokens from sender to recipient
         _transfer(_msgSender(), _to, _value);
         return true;
     }
